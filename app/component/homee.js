57246f06eabb9f3c7cb13083d511eb9a47b2803b
0xdblok <dblok0x@gmail.com>
2024-07-21 22:45:36 +0200

    ee

--- app/component/homee.js
+++ app/component/homee.js
@@ -10,7 +10,7 @@ import birdeye from "../asset/birdeye.jpg";
 import hero from "../asset/hero.jpg";
 export default function Homee() {
   return (
-    <div className="bg-custom-bg pb-8 font-sans ">
+    <div className="bg-custom-bg pb-8 font-mono ">
       <div className="flex justify-center pt-8 ">
         <nav className="flex bg-[#A6B5FF] gap-14 px-10 py-3 rounded-xl text-white font-semibold">
           <a>Buy</a>
